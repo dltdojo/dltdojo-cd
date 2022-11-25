a67ea728c5a91fbddeedcbe8bc566259b2e6943e
y12studio <y12studio@gmail.com>
2022-11-25 10:21:20 +0800

    udpate web500-zk-miden

--- tian/cd22/web500-zk-miden/README.md
+++ tian/cd22/web500-zk-miden/README.md
@@ -2,17 +2,19 @@
 
 [maticnetwork/miden: STARK-based virtual machine](https://github.com/maticnetwork/miden)
 
-# 🍓 101
-
-221123 瀏覽器無法執行
+# 🍓 101 begin push.6 push.5 add end
 
 ```sh
 cargo new --lib zk101
 cd zk101
 # update Cargo.toml src/lib.rs
 wasm-pack build --target web
+```
 
-docker run -i --init -p 8300:3000 --rm -v $PWD:/home:ro busybox:1.35.0 <<EOF
+單獨使用 docker 後開啟瀏覽器測試 ```begin push.6 push.5 add end```
+
+```sh
+docker run -i --init -p 8300:3000 --rm -v $PWD/zk101:/home:ro busybox:1.35.0 <<EOF
 echo http://localhost:8300
 cat <<EOOF > /etc/httpd.conf
 .wasm:application/wasm
@@ -20,4 +22,21 @@ EOOF
 busybox httpd -fv -p 3000 -c /etc/httpd.conf -h /home
 EOF
 ```
+
+# 🐕 102 browser console log testing
+
+只測試最後壹行。起於 HTML 內發出 ```begin push.6 push.5 add end``` 傳入 webassembly 後計算回傳，在於 cypress 中自動分析 console log 確認輸出結果。添加 cypress 減少人工反覆開啟檢視 console log 的流程，以及大量測試不同瀏覽器版本的工作。
+
+```sh
+docker compose -f docker-compose.102.yaml up 
+```
+
+# 🐘 103 one yaml to rule them all
+
+直接在一個 yaml 內修改調整 rust, html, javascript , testing 等。不過反覆的安裝 wasm-pack 與編譯 miden 太花時間還是移到 dockerfile 完成，所以其實是 dockerfile+docker-compose-yaml to rule them all. 
+
+```sh
+docker compose -f docker-compose.103.yaml up 
+```
+
 
--- tian/cd22/web500-zk-miden/zk101/src/lib.rs
+++ tian/cd22/web500-zk-miden/zk101/src/lib.rs
@@ -3,30 +3,30 @@ use wasm_bindgen::prelude::*;
 use miden_assembly::Assembler;
 use miden_prover::{prove, ProgramInputs, ProofOptions};
 
-#[wasm_bindgen]
+#[wasm_bindgen(getter_with_clone)]
 pub struct ProveResult {
-    outputs: Box<[u64]>,
-    proof: Box<[u8]>,
+    pub outputs: Box<[u64]>,
+    pub proof: Box<[u8]>,
+    pub code: String,
 }
 
 #[wasm_bindgen]
-pub fn miden_prove() -> u32 {
-    // instantiate the assembler
+pub fn miden_prove(assembly_code: &str) -> ProveResult {
+    console_error_panic_hook::set_once();
     let assembler = Assembler::default();
-
-    // "begin push.3 push.5 add end"
-    // this is our program, we compile it from assembly code
-    let program = assembler.compile("begin push.3 push.5 add end").unwrap();
-
-    // let's execute it and generate a STARK proof
+    //let program = assembler.compile("begin push.3 push.5 add end").unwrap();
+    let program = assembler.compile(assembly_code).unwrap();
     let (outputs, proof) = prove(
         &program,
         &ProgramInputs::none(),   // we won't provide any inputs
-        1,                        // we'll return one item from the stack
         &ProofOptions::default(), // we'll be using default options
     )
     .unwrap();
-    // assert_eq!(vec![8], outputs);
-    // ProveResult { outputs: outputs.to_vec().into_boxed_slice(), proof: proof.to_bytes().to_vec().into_boxed_slice() }
-    0
+    // println!("Program output: {:?}", outputs.stack_outputs(num_outputs));
+    // assert_eq!(vec![8], outputs.stack_outputs(1));
+    ProveResult {
+        outputs: outputs.stack_outputs(1).to_vec().into_boxed_slice(),
+        proof: proof.to_bytes().to_vec().into_boxed_slice(),
+        code: assembly_code.to_string(),
+    }
 }
