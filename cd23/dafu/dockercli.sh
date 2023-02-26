63eaaaab3b54d814224ad6fe8c31bea62794b14f
y12studio <y12studio@gmail.com>
2023-02-26 16:41:54 +0800

    [cd23/dafu] update dockercli.sh

--- cd23/dafu/dockercli.sh
+++ cd23/dafu/dockercli.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-$DENO_VERSION=1.31.1
-$APPTS_URL=https://raw.githubusercontent.com/dltdojo/dltdojo-cd/v0.1.3/cd23/dafu/app.ts
-$ALLOW_ENV=XDG_DATA_HOME,HOME
-$ALLOW_READ=$HOME/.local/share/dax,/usr/bin/deno,/tmp
-$ALLOW_NET=deno.land,raw.githubusercontent.com
-docker run -i --rm --entrypoint=sh denoland/deno:$DENO_VERSION <<EOF
+DENO_VERSION=1.31.1
+APPTS_URL=https://raw.githubusercontent.com/dltdojo/dltdojo-cd/v0.1.3/cd23/dafu/app.ts
+ALLOW_ENV=XDG_DATA_HOME,HOME
+ALLOW_READ=/root/.local/share/dax,/bin/deno,/usr/bin/deno,/tmp,/app
+ALLOW_WRITE=/root/.local/share/dax
+ALLOW_NET=deno.land,raw.githubusercontent.com
+
+echo "==> 1"
+docker run -i --rm --workdir=/app --entrypoint=sh denoland/deno:$DENO_VERSION <<EOF
 id
 which deno
 env
@@ -13,9 +16,26 @@ deno info
 deno run --allow-env=$ALLOW_ENV \
   --allow-net=$ALLOW_NET \
   --allow-read=$ALLOW_READ  \
+  --allow-write=$ALLOW_WRITE \
   $APPTS_URL
 EOF
 
-docker run -it --rm denoland/deno:$DENO_VERSION run \
-  --allow-env=$ALLOW_ENV --allow-net=$ALLOW_NET --allow-read=$ALLOW_READ  \
+echo "==> 2"
+
+docker run -it --rm --workdir=/app denoland/deno:$DENO_VERSION run \
+  --allow-env=$ALLOW_ENV --allow-net=$ALLOW_NET --allow-read=$ALLOW_READ  --allow-write=$ALLOW_WRITE \
+  $APPTS_URL
+
+echo "==> 3"
+
+DENO_IMG=deno1234
+docker build -t ${DENO_IMG} - <<EOF
+FROM denoland/deno:alpine-$DENO_VERSION
+RUN apk add --no-cache jq openssl curl coreutils
+WORKDIR /app
+RUN deno cache $APPTS_URL
+EOF
+
+docker run -it --rm $DENO_IMG run \
+  --allow-env=$ALLOW_ENV --allow-net=$ALLOW_NET --allow-read=$ALLOW_READ  --allow-write=$ALLOW_WRITE \
   $APPTS_URL
