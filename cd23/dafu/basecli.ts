371ae5799a6b03bb65194a539bc108d793443f21
y12studio <y12studio@gmail.com>
2023-02-26 16:22:38 +0800

    add cilium ingress controller example

--- cd23/dafu/basecli.ts
+++ cd23/dafu/basecli.ts
@@ -1,19 +1,17 @@
 import { Command } from "npm:commander";
 import { readAll } from "https://deno.land/std@0.177.0/streams/read_all.ts";
+import { DemoCiliumIngressNodePort } from "./k8s.ts";
 
 export class BaseCliCommander {
   program = new Command();
 
-  constructor(){
+  constructor() {
     this.program
       .name("foo-cli")
       .description("foo cli")
       .version("0.0.1")
       .option("-d, --debug", "output extra debugging")
       .option("-i, --interactive", "Forces to read the input from the STDIN");
-  }
-
-  parse() {
     this.program
       .command("foostdin")
       .description("foo action")
@@ -25,7 +23,14 @@ export class BaseCliCommander {
           console.log("NO STDIN");
         }
       });
+      this.program
+      .command("demo101").description('cilium ingress controller example')
+      .action(() => {
+        console.log(new DemoCiliumIngressNodePort('dafu101').yaml())
+      });
+  }
 
+  parse() {
     this.program
       .parse();
   }
---
+++ cd23/dafu/k8s.ts
@@ -0,0 +1,112 @@
+import { ApiObject, App, Chart, JsonPatch } from "npm:cdk8s";
+import { Deployment, Ingress, IngressBackend } from "npm:cdk8s-plus-25";
+
+export class Kapp {
+  app: App;
+  chart: Chart;
+  constructor(chartId = "c101") {
+    this.app = new App();
+    this.chart = new Chart(this.app, chartId);
+  }
+  get yaml() {
+    return this.app.synthYaml();
+  }
+}
+
+export const getWhoamiDeplyAndSvc = (chart: Chart) => {
+  const svc = new Deployment(chart, "whoami", {
+    replicas: 2,
+    containers: [{
+      image: "traefik/whoami",
+      portNumber: 80,
+      securityContext: {
+        ensureNonRoot: false,
+      },
+    }],
+  }).exposeViaService({
+    ports: [
+      {
+        port: 80,
+      },
+    ],
+  });
+
+  return svc;
+};
+
+export const getNginxDeplyAndSvc = (chart: Chart) => {
+  const svc = new Deployment(chart, "nginx", {
+    replicas: 2,
+    containers: [{
+      image: "nginx",
+      portNumber: 80,
+      securityContext: {
+        ensureNonRoot: false,
+        readOnlyRootFilesystem: false,
+      },
+    }],
+  }).exposeViaService({
+    ports: [
+      {
+        port: 80,
+      },
+    ],
+  });
+
+  return svc;
+};
+
+export const getNodePortIngress = (chart: Chart) => {
+  const ingress = new Ingress(chart, "ing", {
+    metadata: {
+      annotations: {
+        "io.cilium.ingress/service-type": "NodePort",
+        "io.cilium.ingress/insecure-node-port": "32080",
+        // "io.cilium.ingress/secure-node-port": "32443",
+        "io.cilium.ingress/loadbalancer-mode": "dedicated",
+      },
+    },
+  });
+
+  // IngressClassName only in cdk8splus26/k8s/IngressSpec.go
+  // https://github.com/search?q=org%3Acdk8s-team+ingressClassName&type=code
+  // https://cdk8s.io/docs/latest/basics/escape-hatches/#patching-api-objects-directly
+  //
+  const kubeIngress = ApiObject.of(ingress);
+  kubeIngress.addJsonPatch(JsonPatch.add("/spec/ingressClassName", "cilium"));
+  return ingress;
+};
+
+export class DemoCiliumIngressNodePort {
+  appName: string;
+  kapp: Kapp;
+
+  constructor(name: string) {
+    this.appName = name || "k101";
+    this.kapp = new Kapp(this.appName);
+    const svcWhoami = getWhoamiDeplyAndSvc(this.kapp.chart);
+    const svcNginx = getNginxDeplyAndSvc(this.kapp.chart);
+    //
+    // https://docs.cilium.io/en/stable/network/servicemesh/ingress/
+    // https://docs.cilium.io/en/stable/network/servicemesh/http/
+    // [ingress: Support NodePort for dedicated Ingress by sayboras · Pull Request #22974 · cilium/cilium](https://github.com/cilium/cilium/pull/22974)
+    //
+    const ingress = getNodePortIngress(this.kapp.chart);
+
+    ingress.addHostRule(
+      "dev101.default.svc",
+      "/",
+      IngressBackend.fromService(svcWhoami),
+    );
+    ingress.addHostRule(
+      "dev201.default.svc",
+      "/",
+      IngressBackend.fromService(svcNginx),
+    );
+    ingress.addDefaultBackend(IngressBackend.fromService(svcWhoami));
+  }
+
+  yaml() {
+    return this.kapp.yaml;
+  }
+}
